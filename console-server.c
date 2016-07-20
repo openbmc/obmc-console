a8e67604310edc99ba2538865845ea7352c6cc48
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2016-07-20 05:35:45 -0500

    Supply Serial port as an optional argument to obmc-console-server
    obmc-console-server is a serial console server that currently uses
    ttyS5 on the BMC and this is configured in obmc-console.conf.
    
    This patch introduced a new parameter so that tty port can be specified
    while starting the server. When this parameter is passed, it overrides
    the TTY that has been given in the configuration file.
    
    This feature is being added to support systemd template unit files.
    
    We would write a 'obmc-console@.service' file and then start
    'obmc-console@ttyS5' and the part after the @ becomes a parameter to the unit
    that can be passed along. This is only "useful" if we have somewhere to pass it
    to.
    
    One aspect of the template unit, which is appropriate to this issue, is that the
    template parameter can be used as a dependency, so we can ensure the service
    obmc-console@%s will not start unless /dev/%s is present.
    
    Below are the advantages of this approach:
    
    	1. We can use systemd template unit files as intended without hard-coding the
    	   dev-entry.
    
    	2. It gets us very close to being able to handle multiple host consoles for a
    	   blade-chassis style system.
    
    	3. It allows us to easily map a [virtual] physical UART in QEMU to the
    	   obmc-console application for test purposes.
    
    Change-Id: I6599a2572e2db996fba9a19a3d65ff502d550114
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- console-server.c
+++ console-server.c
@@ -74,7 +74,9 @@ static void usage(const char *progname)
 "usage: %s [options]\n"
 "\n"
 "Options:\n"
-"  --config <FILE>  Use FILE for configuration\n"
+"  --config <FILE>         Use FILE for configuration\n"
+"  --tty    <tty device>   Use this tty as the input device\
+                           (overrides configuration file)\n"
 "",
 		progname);
 }
@@ -215,8 +217,6 @@ static int tty_init(struct console *console, struct config *config)
 	const char *val;
 	char *endp;
 	int rc;
-
-	console->tty_kname = config_get_value(config, "device");
 
 	val = config_get_value(config, "lpc-address");
 	if (val) {
@@ -498,28 +498,33 @@ int run_console(struct console *console)
 }
 static const struct option options[] = {
 	{ "config",	required_argument,	0, 'c'},
+	{ "tty",	required_argument,	0, 't'},
 	{ 0,  0, 0, 0},
 };
 
 int main(int argc, char **argv)
 {
 	const char *config_filename = NULL;
+	const char *config_tty_kname = NULL;
 	struct console *console;
 	struct config *config;
 	int rc;
 
 	rc = -1;
+	int c, idx;
 
 	for (;;) {
-		int c, idx;
 
-		c = getopt_long(argc, argv, "c:", options, &idx);
+		c = getopt_long(argc, argv, "c:t:", options, &idx);
 		if (c == -1)
 			break;
 
 		switch (c) {
 		case 'c':
 			config_filename = optarg;
+			break;
+		case 't':
+			config_tty_kname = optarg;
 			break;
 		case 'h':
 		case '?':
@@ -538,6 +543,11 @@ int main(int argc, char **argv)
 		warnx("Can't read configuration, exiting.");
 		goto out_free;
 	}
+
+	if (config_tty_kname)
+		console->tty_kname = config_tty_kname;
+	else
+		console->tty_kname = config_get_value(config, "device");
 
 	rc = tty_init(console, config);
 	if (rc)
