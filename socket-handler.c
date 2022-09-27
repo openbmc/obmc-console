8c21ada146c4f3352bc45982a49354c745438d93
Tingting Chen <chentingting.2150@bytedance.com>
2022-09-27 18:28:00 +0800

    obmc-console: Add sysrq handle in sol socket handle.
    Polling MSG_OOB flag to sendbreak to os to trigger sysrq function.
    
    Tested:
    1. Open SOL by ipmitool: ipmitool -H <bmcip> -I lanp -U <user> -P <password> sol activate
    2. In SOL terminal, login os and "echo 1 > /proc/sys/kernel/sysrq" to open sysrq function
    3. In SOL terminal, Enter ~+B to generate break, then enter "h" in 5 second.
    4. Veirfy below message shown in SOL:
    root@bytedance:~# ~B [send break]
    [  366.377331] sysrq: HELP : loglevel(0-9) reboot(b) crash(c) terminate-all-tasks(e)
    memory-full-oom-kill(f) kill-all-tasks(i) thaw-filesystems(j) sak(k) show-backtrace-all-active-cpus(l)
    show-memory-usage(m) nice-all-RT-tasks(n) poweroff(o) show-registers(p) show-all-timers(q) unraw(r)
    sync(s) show-task-states(t) unmount(u) force-fb(V) show-blocked-tasks(w) dump-ftrace-buffer(z)
    
    Change-Id: I5d67611788d2e9d60195219b2d3cfd7fb715eca4
    Signed-off-by: Tingting Chen <chentingting.2150@bytedance.com>

--- socket-handler.c
+++ socket-handler.c
@@ -238,6 +238,22 @@ static enum poller_ret client_poll(struct handler *handler,
 	uint8_t buf[4096];
 	int rc;
 
+	if (events & POLLPRI) {
+		rc = recv(client->fd, buf, sizeof(buf), MSG_OOB);
+		if (rc < 0) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+				return POLLER_OK;
+			else
+				goto err_close;
+		}
+		if (rc == 0)
+			goto err_close;
+		if(buf[0] == 72)//used to send break to os(the data need match with sol client send)
+		{
+			console_send_break(sh->console);
+		}
+	}
+
 	if (events & POLLIN) {
 		rc = recv(client->fd, buf, sizeof(buf), MSG_DONTWAIT);
 		if (rc < 0) {
@@ -287,7 +303,7 @@ static enum poller_ret socket_poll(struct handler *handler,
 	client->sh = sh;
 	client->fd = fd;
 	client->poller = console_poller_register(sh->console, handler,
-			client_poll, client_timeout, client->fd, POLLIN,
+			client_poll, client_timeout, client->fd, POLLIN|POLLPRI,
 			client);
 	client->rbc = console_ringbuffer_consumer_register(sh->console,
 			client_ringbuffer_poll, client);
