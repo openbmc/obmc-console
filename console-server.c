89f742e2849560326ffc51a7dc5c7b8aad11dbe4
Ninad Palsule <ninadpalsule@us.ibm.com>
2023-03-31 09:49:00 -0500

    Added new dbus interface to query console info
    obmc-console recipe configure consoles by setting the socket-id
    field in the console server config file. obmc-console server use this
    socket id to build a abract socket name and register new service
    "xyz.openbmc_project.Console.Access". The leaf of the object path will
    be socket-id configured. It also exposes the unix abstract socket name
    through SocketName property. The socket name is sent as a byte stream as
    abstract socket contains null character at the start.
    
    For example if recipe configured socket-id as "console0" the following object is
    exported through dbus interface
    
        # Tree for "console0" console.
        busctl tree xyz.openbmc_project.Console.console0
        `-/xyz
          `-/xyz/openbmc_project
            `-/xyz/openbmc_project/console
              `-/xyz/openbmc_project/console/console0
    
        # Introspect for console0 object.
        $ busctl get-property xyz.openbmc_project.Console.console0 /xyz/openbmc_project/console/console0 xyz.openbmc_project.Console.Access SocketName
        ay 22 0 111 98 109 99 45 99 111 110 115 111 108 101 46 99 111 110 115 111 108 101 48
    
        # Tree for "hypervisor" console
        busctl tree xyz.openbmc_project.Console.hypervisor
        `-/xyz
          `-/xyz/openbmc_project
            `-/xyz/openbmc_project/console
              `-/xyz/openbmc_project/console/hypervisor
    
        # Introspect the hypervisor object
        $ busctl get-property xyz.openbmc_project.Console.hypervisor /xyz/openbmc_project/console/hypervisor xyz.openbmc_project.Console.Access SocketName
        ay 24 0 111 98 109 99 45 99 111 110 115 111 108 101 46 104 121 112 101 114 118 105 115 111 114
    
        # Note that this is an example of two consoles one is 'console0' and
        # second is 'hypervisor'. I have formated the output to easy viewing.
        # We also have one extra service for console that is coming from the
        # service exist today to get/set the tty device baud rate.
        $ busctl call  xyz.openbmc_project.ObjectMapper \
            /xyz/openbmc_project/object_mapper xyz.openbmc_project.ObjectMapper \
            GetSubTree sias /xyz/openbmc_project/console 0 1 \
            xyz.openbmc_project.Console.Access
        a{sa{sas}}
        2
        "/xyz/openbmc_project/console/console0" \
            1
            "xyz.openbmc_project.Console.console0"
               4 "org.freedesktop.DBus.Introspectable"
                 "org.freedesktop.DBus.Peer"
                 "org.freedesktop.DBus.Properties"
                 "xyz.openbmc_project.Console.Access"
        "/xyz/openbmc_project/console/hypervisor"
            2
            "xyz.openbmc_project.Console.hypervisor"
               4 "org.freedesktop.DBus.Introspectable"
                 "org.freedesktop.DBus.Peer"
                 "org.freedesktop.DBus.Properties"
                 "xyz.openbmc_project.Console.Access"
            "xyz.openbmc_project.console"
              4 "org.freedesktop.DBus.Introspectable"
                "org.freedesktop.DBus.Peer"
                "org.freedesktop.DBus.Properties"
                "xyz.openbmc_project.Console.Access"
    
    Tested:
        Tested on the rainer system
    
    Related commits:
        1) phosphor-dbus-interface: https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/61486
        2) obmc-console: https://gerrit.openbmc.org/c/openbmc/obmc-console/+/62496
        3) bmcweb: https://gerrit.openbmc.org/c/openbmc/bmcweb/+/62525
    
    Change-Id: Ifb70ce5585c3937f3abd904ffbae51ca67f58724
    Signed-off-by: Ninad Palsule <ninadpalsule@us.ibm.com>

--- console-server.c
+++ console-server.c
@@ -34,22 +34,31 @@
 
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/socket.h>
 #include <poll.h>
 
 #include "console-server.h"
 
-#define DBUS_ERR  "org.openbmc.error"
-#define DBUS_NAME "xyz.openbmc_project.console"
-#define OBJ_NAME  "/xyz/openbmc_project/console"
+#define DBUS_ERR	  "org.openbmc.error"
+#define DBUS_NAME	  "xyz.openbmc_project.console"
+#define OBJ_NAME	  "/xyz/openbmc_project/console"
+#define CONSOLE_DBUS_NAME "xyz.openbmc_project.Console.%s"
+#define CONSOLE_OBJ_NAME  "/xyz/openbmc_project/console/%s"
+#define CONSOLE_INTF_NAME "xyz.openbmc_project.Console.Access"
 
 struct console {
 	const char *tty_kname;
 	char *tty_sysfs_devnode;
 	char *tty_dev;
+	const char *console_id;
 	int tty_sirq;
 	uint16_t tty_lpc_addr;
 	speed_t tty_baud;
 	int tty_fd;
+
+	/* Socket name starts with null character hence we need length */
+	socket_path_t socket_name;
+	ssize_t socket_name_len;
 
 	struct ringbuffer *rb;
 
@@ -81,6 +90,9 @@ enum internal_pollfds {
 
 /* size of the shared backlog ringbuffer */
 const size_t buffer_size = 128ul * 1024ul;
+
+/* size of the dbus object path length */
+const size_t dbus_obj_path_len = 1024;
 
 /* state shared with the signal handler */
 static bool sigint;
@@ -417,11 +429,33 @@ static int get_handler(sd_bus *bus __attribute__((unused)),
 	return r;
 }
 
+static int get_socket_name(sd_bus *bus __attribute__((unused)),
+			   const char *path __attribute__((unused)),
+			   const char *interface __attribute__((unused)),
+			   const char *property __attribute__((unused)),
+			   sd_bus_message *reply, void *userdata,
+			   sd_bus_error *error __attribute__((unused)))
+{
+	struct console *console = userdata;
+
+	/* The abstract socket name starts with null character hence we need to
+	 * send it as a byte stream instead of regular string.
+	 */
+	return sd_bus_message_append_array(reply, 'y', console->socket_name,
+					   console->socket_name_len);
+}
+
 static const sd_bus_vtable console_vtable[] = {
 	SD_BUS_VTABLE_START(0),
 	SD_BUS_METHOD("setBaudRate", "u", "x", method_set_baud_rate,
 		      SD_BUS_VTABLE_UNPRIVILEGED),
 	SD_BUS_PROPERTY("baudrate", "u", get_handler, 0, 0),
+	SD_BUS_VTABLE_END,
+};
+
+static const sd_bus_vtable supported_consoles_vtable[] = {
+	SD_BUS_VTABLE_START(0),
+	SD_BUS_PROPERTY("SocketName", "ay", get_socket_name, 0, 0),
 	SD_BUS_VTABLE_END,
 };
 
@@ -431,6 +465,9 @@ static void dbus_init(struct console *console,
 	int dbus_poller = 0;
 	int fd;
 	int r;
+	size_t bytes;
+	char console_obj_name[dbus_obj_path_len];
+	char console_bus_name[dbus_obj_path_len];
 
 	if (!console) {
 		warnx("Couldn't get valid console");
@@ -458,6 +495,37 @@ static void dbus_init(struct console *console,
 		return;
 	}
 
+	/* Register support console interface */
+	bytes = snprintf(console_obj_name, dbus_obj_path_len, CONSOLE_OBJ_NAME,
+			 console->console_id);
+	if (bytes >= dbus_obj_path_len) {
+		warnx("Console id '%s' is too long. There is no enough space in the buffer.",
+		      console->console_id);
+		return;
+	}
+	r = sd_bus_add_object_vtable(console->bus, NULL, console_obj_name,
+				     CONSOLE_INTF_NAME,
+				     supported_consoles_vtable, console);
+	if (r < 0) {
+		warnx("Failed to issue method call: %s", strerror(-r));
+		return;
+	}
+
+	bytes = snprintf(console_bus_name, dbus_obj_path_len, CONSOLE_DBUS_NAME,
+			 console->console_id);
+	if (bytes >= dbus_obj_path_len) {
+		warnx("Console id '%s' is too long. There is no enough space in the buffer.",
+		      console->console_id);
+		return;
+	}
+	r = sd_bus_request_name(console->bus, console_bus_name,
+				SD_BUS_NAME_ALLOW_REPLACEMENT |
+					SD_BUS_NAME_REPLACE_EXISTING);
+	if (r < 0) {
+		warnx("Failed to acquire service name: %s", strerror(-r));
+		return;
+	}
+
 	fd = sd_bus_get_fd(console->bus);
 	if (fd < 0) {
 		warnx("Couldn't get the bus file descriptor");
@@ -468,6 +536,44 @@ static void dbus_init(struct console *console,
 
 	console->pollfds[dbus_poller].fd = fd;
 	console->pollfds[dbus_poller].events = POLLIN;
+}
+
+/* Read console if from config and prepare a socket name */
+static int set_socket_info(struct console *console, struct config *config)
+{
+	struct sockaddr_un addr;
+	socket_path_t socketName;
+	size_t addrlen;
+	ssize_t len;
+	char *name;
+
+	console->console_id = config_get_value(config, "socket-id");
+	if (!console->console_id) {
+		warnx("Error: The socket-id is not set in the config file");
+		return EXIT_FAILURE;
+	}
+
+	/* Create a dummy socket address to build the path */
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	len = console_socket_path(&addr, console->console_id);
+	if (len < 0) {
+		warn("Failed to set socket path: %s", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	/* Now get the socket name string from address and save it in the console */
+	addrlen = sizeof(addr) - sizeof(addr.sun_path) + len;
+	console_socket_path_readable(&addr, addrlen, socketName);
+
+	/* Save the abstract socket name */
+	name = &console->socket_name[0];
+	memcpy(name + 1, socketName, len);
+	name[0] = '\0';
+
+	console->socket_name_len = len;
+
+	return 0;
 }
 
 static void handlers_init(struct console *console, struct config *config)
@@ -897,6 +1003,10 @@ int main(int argc, char **argv)
 	if (!config_tty_kname) {
 		warnx("No TTY device specified");
 		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if (set_socket_info(console, config)) {
 		return EXIT_FAILURE;
 	}
 
