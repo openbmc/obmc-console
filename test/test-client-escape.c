f2232d5fe8d00384710400e1db0996dcfc5ecb4d
Andrew Jeffery <andrew@aj.id.au>
2023-04-18 21:50:17 +0930

    obmc-console: Fix bugprone-macro-parentheses
    For example:
    
    ```
    /usr/bin/clang-tidy -checks=-*, bugprone-macro-parentheses -export-fixes /tmp/tmpzth2fheg/tmp9x7dpgyc.yaml -p=build /mnt/host/andrew/home/andrew/src/openbmc/obmc-console/console-server.c
    /mnt/host/andrew/home/andrew/src/openbmc/obmc-console/build/../console-server.h:150:43: error: macro argument should be enclosed in parentheses [bugprone-macro-parentheses,-warnings-as-errors]
                                              ^
                                              ()
    ```
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: I10b93e2b3e16b51bcdf2adf113a38e67bce37e88

--- test/test-client-escape.c
+++ test/test-client-escape.c
@@ -150,7 +150,7 @@ struct test tests[] = {
 	},
 };
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 struct test_ctx ctxs[ARRAY_SIZE(tests)];
 
