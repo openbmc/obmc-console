openbmc/obmc-console

commit 4e71869185995ff44e4bfafb8be1e16452e87835

Fixed broken dbus interface for multiple consoles
There is a console server instance per console. So if there are two
consoles then two console server instances will be started. The issue
is that each instance is sharing same bus name and object path. The
issue is fixed by keeping the interface name same as before
(xyz.openbmc_project.console) but changing the bus name and object path
name by appending socket-id which makes them unique. So the bus name
becomes xyz.openbmc_project.Console.<socket-id> and the object name
becomes /xyz/openbmc_project/Console/<socket-id>. Each console server
has its own socket-id hence there should not be any conflict between two
consoles.

Testing:
    -> Ran busctl to check object changes
    $ busctl get-property xyz.openbmc_project.Console.console0 /xyz/openbmc_project/console/console0 xyz.openbmc_project.console baudrate
    u 0
    $ busctl introspect xyz.openbmc_project.Console.console0 /xyz/openbmc_project/console/console0    ....
    xyz.openbmc_project.Console         interface -         -            -
    .setBaudRate                        method    u         x            -
    .baudrate                           property  u         0            -

Change-Id: Ic1aacd8f284a867416081b4965b55ba7c3cb34e3
Signed-off-by: Ninad Palsule <ninadpalsule@us.ibm.com>

console-server.c
@@ -38,14 +38,19 @@
 
 #include "console-server.h"
 
+/* size of the dbus object path length */
+const size_t dbus_obj_path_len = 1024;
+
 #define DBUS_ERR  "org.openbmc.error"
-#define DBUS_NAME "xyz.openbmc_project.console"
-#define OBJ_NAME  "/xyz/openbmc_project/console"
+#define INTF_NAME "xyz.openbmc_project.console"
+#define DBUS_NAME "xyz.openbmc_project.Console.%s"
+#define OBJ_NAME  "/xyz/openbmc_project/console/%s"
 
 struct console {
 	const char *tty_kname;
 	char *tty_sysfs_devnode;
 	char *tty_dev;
+	const char *console_id;
 	int tty_sirq;
 	uint16_t tty_lpc_addr;
 	speed_t tty_baud;
@@ -428,9 +433,12 @@ static const sd_bus_vtable console_vtable[] = {
 static void dbus_init(struct console *console,
 		      struct config *config __attribute__((unused)))
 {
+	char obj_name[dbus_obj_path_len];
+	char dbus_name[dbus_obj_path_len];
 	int dbus_poller = 0;
 	int fd;
 	int r;
+	size_t bytes;
 
 	if (!console) {
 		warnx("Couldn't get valid console");
@@ -443,14 +451,31 @@ static void dbus_init(struct console *console,
 		return;
 	}
 
-	r = sd_bus_add_object_vtable(console->bus, NULL, OBJ_NAME, DBUS_NAME,
+	/* Register support console interface */
+	bytes = snprintf(obj_name, dbus_obj_path_len, OBJ_NAME,
+			 console->console_id);
+	if (bytes >= dbus_obj_path_len) {
+		warnx("Console id '%s' is too long. There is no enough space in the buffer.",
+		      console->console_id);
+		return;
+	}
+
+	r = sd_bus_add_object_vtable(console->bus, NULL, obj_name, INTF_NAME,
 				     console_vtable, console);
 	if (r < 0) {
 		warnx("Failed to issue method call: %s", strerror(-r));
 		return;
 	}
 
-	r = sd_bus_request_name(console->bus, DBUS_NAME,
+	bytes = snprintf(dbus_name, dbus_obj_path_len, DBUS_NAME,
+			 console->console_id);
+	if (bytes >= dbus_obj_path_len) {
+		warnx("Console id '%s' is too long. There is no enough space in the buffer.",
+		      console->console_id);
+		return;
+	}
+
+	r = sd_bus_request_name(console->bus, dbus_name,
 				SD_BUS_NAME_ALLOW_REPLACEMENT |
 					SD_BUS_NAME_REPLACE_EXISTING);
 	if (r < 0) {
@@ -901,6 +926,12 @@ int main(int argc, char **argv)
 	}
 
 	console->tty_kname = config_tty_kname;
+
+	console->console_id = config_get_value(config, "socket-id");
+	if (!console->console_id) {
+		warnx("Error: The socket-id is not set in the config file");
+		return EXIT_FAILURE;
+	}
 
 	rc = tty_init(console, config);
 	if (rc) {

commit b167c98e2062b76613efbe25010f64283f02b063

obmc-console: Provide a default value for `console-id`.
4e7186918599 ("Fixed broken dbus interface for multiple consoles")
introduced the requirement that `console-id` be specified in the
configuration files for both the client and server. It was paired with a
fix to platform configurations in the OpenBMC bitbake metadata[1]. In
theory this should have worked, but because specifying `console-id`
wasn't a requirement, not all platforms supplied a client configuration.
Instead they relied on the default behaviour.

[1]: https://gerrit.openbmc.org/c/openbmc/openbmc/+/62712

Remove the requirement that a `console-id` be specified and instead
provide a default value that can be overridden by configuration. This
carries forward the consequence from 4e7186918599 ("Fixed broken dbus
interface for multiple consoles") that the original `\0obmc-console`
abstract socket will never be created. This doesn't resolve the break in
ipmid or bmcweb, but resolves the break to SSH-based SOL on platforms
not supplying client configuration files for one of their consoles.

The fix to bmcweb (whose strategy can also be applied to ipmid) is
currently being prototyped[2].

[2]: https://discord.com/channels/775381525260664832/1083551792094249051/1103867159412752424

A deeper treatment of the problems, impacts, and solutions is provided
in [3].

[3]: https://amboar.github.io/notes/2023/05/08/happenings-in-obmc-console.html

Fixes: 4e7186918599 ("Fixed broken dbus interface for multiple consoles")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I970578f1b695f729f6524c4da6bba6e89bf14d52

console-server.c
@@ -406,11 +406,6 @@ static int set_socket_info(struct console *console, struct config *config,
 		}
 	}
 
-	if (!resolved_id) {
-		warnx("console-id was not specified");
-		return EXIT_FAILURE;
-	}
-
 	console->console_id = resolved_id;
 
 	/* Get the socket name/path */

console-socket.c
@@ -16,6 +16,7 @@
 
 #include "console-server.h"
 
+#include <err.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
@@ -32,14 +33,12 @@ ssize_t console_socket_path(socket_path_t sun_path, const char *id)
 {
 	ssize_t rc;
 
-	if (id) {
-		rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1,
-			      CONSOLE_SOCKET_PREFIX ".%s", id);
-	} else {
-		rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1,
-			      CONSOLE_SOCKET_PREFIX);
+	if (!id) {
+		id = "default";
+		warnx("console-id was not specified, using '%s'", id);
 	}
 
+	rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1, CONSOLE_SOCKET_PREFIX ".%s", id);
 	if (rc < 0) {
 		return rc;
 	}