openbmc/obmc-console

commit ae2460d0b8e808078ba130d63514b20b66cd3375

obmc-console: Provide a default value for `console-id`.
4e7186918599 ("Fixed broken dbus interface for multiple consoles")
introduced the requirement that `console-id` be specified in the
configuration files for both the client and server. It was paired with a
fix to platform configurations in the OpenBMC bitbake metadata[1]. In
theory this should have worked, but because specifying `console-id`
wasn't a requirement, not all platforms supplied a client configuration.
Instead they relied on the default behaviour.

[1]: https://gerrit.openbmc.org/c/openbmc/openbmc/+/62712

Remove the requirement that a `console-id` be specified and instead
provide a default value that can be overridden by configuration. This
carries forward the consequence from 4e7186918599 ("Fixed broken dbus
interface for multiple consoles") that the original `\0obmc-console`
abstract socket will never be created. This doesn't resolve the break in
ipmid or bmcweb, but resolves the break to SSH-based SOL on platforms
not supplying client configuration files for one of their consoles.

The fix to bmcweb (whose strategy can also be applied to ipmid) is
currently being prototyped[2].

[2]: https://discord.com/channels/775381525260664832/1083551792094249051/1103867159412752424

A deeper treatment of the problems, impacts, and solutions is provided
in [3].

[3]: https://amboar.github.io/notes/2023/05/08/happenings-in-obmc-console.html

Fixes: 4e7186918599 ("Fixed broken dbus interface for multiple consoles")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I970578f1b695f729f6524c4da6bba6e89bf14d52

console-server.c
@@ -409,11 +409,6 @@ static int set_socket_info(struct console *console, struct config *config,
 		}
 	}
 
-	if (!resolved_id) {
-		warnx("console-id was not specified");
-		return EXIT_FAILURE;
-	}
-
 	console->console_id = resolved_id;
 
 	/* Get the socket name/path */

console-socket.c
@@ -16,6 +16,7 @@
 
 #include "console-server.h"
 
+#include <err.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdio.h>
@@ -32,14 +33,12 @@ ssize_t console_socket_path(socket_path_t sun_path, const char *id)
 {
 	ssize_t rc;
 
-	if (id) {
-		rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1,
-			      CONSOLE_SOCKET_PREFIX ".%s", id);
-	} else {
-		rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1,
-			      CONSOLE_SOCKET_PREFIX);
+	if (!id) {
+		id = "default";
 	}
 
+	rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1,
+		      CONSOLE_SOCKET_PREFIX ".%s", id);
 	if (rc < 0) {
 		return rc;
 	}

commit 43b0f16f5094dc76f1dd522cf4f57a135565753b

obmc-console: Consolidate handling of default socket-id
If console-id is not specified on command line or in the config file
then use the default value. ae2460d0b8e8 ("obmc-console: Provide a
default value for `console-id`.") only implemented the default value for
naming the abstract listening socket and overlooked the new DBus path
naming convention.  This caused issues during dbus registration:

```
obmc-console-server: Object name: /xyz/openbmc_project/console/(null)
obmc-console-server: Failed to issue method call: Invalid argument
```

Fixes: ae2460d0b8e8 ("obmc-console: Provide a default value for `console-id`.")
Change-Id: I6d0f7b23cc085992189cd4463129a6aae590b3e7
Signed-off-by: Ninad Palsule <ninadpalsule@us.ibm.com>
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

config.c
@@ -332,6 +332,29 @@ int config_parse_logsize(const char *size_str, size_t *size)
 	return 0;
 }
 
+/* Default console id if not specified on command line or in config */
+#define DEFAULT_CONSOLE_ID "default"
+
+/* Get the console id */
+const char *config_resolve_console_id(struct config *config, const char *id_arg)
+{
+	const char *configured;
+
+	if (id_arg) {
+		return id_arg;
+	}
+
+	if ((configured = config_get_value(config, "console-id"))) {
+		return configured;
+	}
+
+	if ((configured = config_get_value(config, "socket-id"))) {
+		return configured;
+	}
+
+	return DEFAULT_CONSOLE_ID;
+}
+
 #ifdef CONFIG_TEST
 int main(void)
 {

console-client.c
@@ -212,8 +212,10 @@ static int client_tty_init(struct console_client *client)
 	return 0;
 }
 
-static int client_init(struct console_client *client, const char *console_id)
+static int client_init(struct console_client *client, struct config *config,
+		       const char *console_id)
 {
+	const char *resolved_id = NULL;
 	struct sockaddr_un addr;
 	socket_path_t path;
 	ssize_t len;
@@ -225,9 +227,12 @@ static int client_init(struct console_client *client, const char *console_id)
 		return -1;
 	}
 
+	/* Get the console id */
+	resolved_id = config_resolve_console_id(config, console_id);
+
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_UNIX;
-	len = console_socket_path(addr.sun_path, console_id);
+	len = console_socket_path(addr.sun_path, resolved_id);
 	if (len < 0) {
 		if (errno) {
 			warn("Failed to configure socket: %s", strerror(errno));
@@ -325,15 +330,6 @@ int main(int argc, char *argv[])
 			esc = (const uint8_t *)config_get_value(
 				config, "escape-sequence");
 		}
-
-		if (!console_id) {
-			console_id = config_get_value(config, "console-id");
-		}
-
-		/* socket-id is deprecated */
-		if (!console_id) {
-			console_id = config_get_value(config, "socket-id");
-		}
 	}
 
 	if (esc) {
@@ -341,7 +337,7 @@ int main(int argc, char *argv[])
 		client->esc_state.str.str = esc;
 	}
 
-	rc = client_init(client, console_id);
+	rc = client_init(client, config, console_id);
 	if (rc) {
 		goto out_config_fini;
 	}

console-server.c
@@ -391,28 +391,17 @@ int console_data_out(struct console *console, const uint8_t *data, size_t len)
 	return write_buf_to_fd(console->tty.fd, data, len);
 }
 
-/* Read console if from config and prepare a socket name */
+/* Prepare a socket name */
 static int set_socket_info(struct console *console, struct config *config,
 			   const char *console_id)
 {
-	const char *resolved_id;
 	ssize_t len;
 
-	if (console_id) {
-		resolved_id = console_id;
-	} else {
-		resolved_id = config_get_value(config, "console-id");
-
-		/* socket-id is deprecated */
-		if (!resolved_id) {
-			resolved_id = config_get_value(config, "socket-id");
-		}
-	}
-
-	console->console_id = resolved_id;
+	/* Get console id */
+	console->console_id = config_resolve_console_id(config, console_id);
 
 	/* Get the socket name/path */
-	len = console_socket_path(console->socket_name, resolved_id);
+	len = console_socket_path(console->socket_name, console->console_id);
 	if (len < 0) {
 		warn("Failed to set socket path: %s", strerror(errno));
 		return EXIT_FAILURE;

console-server.h
@@ -209,6 +209,8 @@ void tty_init_termios(struct console *console);
 struct config;
 const char *config_get_value(struct config *config, const char *name);
 struct config *config_init(const char *filename);
+const char *config_resolve_console_id(struct config *config,
+				      const char *id_arg);
 void config_fini(struct config *config);
 
 int config_parse_baud(speed_t *speed, const char *baud_string);

console-socket.c
@@ -34,7 +34,8 @@ ssize_t console_socket_path(socket_path_t sun_path, const char *id)
 	ssize_t rc;
 
 	if (!id) {
-		id = "default";
+		errno = EINVAL;
+		return -1;
 	}
 
 	rc = snprintf(sun_path + 1, sizeof(socket_path_t) - 1,

test/meson.build
@@ -2,6 +2,7 @@ tests = [
 	'test-client-escape',
 	'test-config-parse',
 	'test-config-parse-logsize',
+	'test-config-resolve-console-id',
 	'test-ringbuffer-boundary-poll',
 	'test-ringbuffer-boundary-read',
 	'test-ringbuffer-contained-offset-read',

test/test-config-resolve-console-id.c
@@ -1,0 +1,140 @@
+#include <assert.h>
+
+#define TEST_CONSOLE_ID "test"
+
+#include "config.c"
+
+static void test_independence_cmdline_optarg(void)
+{
+	const char *console_id;
+	struct config *ctx;
+
+	ctx = calloc(1, sizeof(*ctx));
+	console_id = config_resolve_console_id(ctx, TEST_CONSOLE_ID);
+
+	assert(!strcmp(console_id, TEST_CONSOLE_ID));
+
+	config_fini(ctx);
+}
+
+static void test_independence_config_console_id(void)
+{
+	const char *console_id;
+	struct config *ctx;
+	char *buf;
+
+	ctx = calloc(1, sizeof(*ctx));
+	buf = strdup("socket-id = " TEST_CONSOLE_ID);
+	config_parse(ctx, buf);
+	free(buf);
+	console_id = config_resolve_console_id(ctx, NULL);
+
+	assert(!strcmp(console_id, TEST_CONSOLE_ID));
+
+	config_fini(ctx);
+}
+
+static void test_independence_config_socket_id(void)
+{
+	const char *console_id;
+	struct config *ctx;
+	char *buf;
+
+	ctx = calloc(1, sizeof(*ctx));
+	buf = strdup("socket-id = " TEST_CONSOLE_ID);
+	config_parse(ctx, buf);
+	free(buf);
+	console_id = config_resolve_console_id(ctx, NULL);
+
+	assert(!strcmp(console_id, TEST_CONSOLE_ID));
+
+	config_fini(ctx);
+}
+
+static void test_independence_default(void)
+{
+	const char *console_id;
+	struct config *ctx;
+
+	ctx = calloc(1, sizeof(*ctx));
+	console_id = config_resolve_console_id(ctx, NULL);
+
+	assert(!strcmp(console_id, DEFAULT_CONSOLE_ID));
+
+	config_fini(ctx);
+}
+
+static void test_precedence_cmdline_optarg(void)
+{
+	static const char *const config = "console-id = console\n"
+					  "socket-id = socket\n";
+	const char *console_id;
+	struct config *ctx;
+	char *buf;
+
+	ctx = calloc(1, sizeof(*ctx));
+	buf = strdup(config);
+	config_parse(ctx, buf);
+	free(buf);
+	console_id = config_resolve_console_id(ctx, TEST_CONSOLE_ID);
+
+	assert(config_get_value(ctx, "console-id"));
+	assert(config_get_value(ctx, "socket-id"));
+	assert(!strcmp(console_id, TEST_CONSOLE_ID));
+
+	config_fini(ctx);
+}
+
+static void test_precedence_config_console_id(void)
+{
+	static const char *const config = "console-id = console\n"
+					  "socket-id = socket\n";
+	const char *console_id;
+	struct config *ctx;
+	char *buf;
+
+	ctx = calloc(1, sizeof(*ctx));
+	buf = strdup(config);
+	config_parse(ctx, buf);
+	free(buf);
+	console_id = config_resolve_console_id(ctx, NULL);
+
+	assert(config_get_value(ctx, "console-id"));
+	assert(config_get_value(ctx, "socket-id"));
+	assert(!strcmp(console_id, "console"));
+
+	config_fini(ctx);
+}
+
+static void test_precedence_config_socket_id(void)
+{
+	static const char *const config = "socket-id = socket\n";
+	const char *console_id;
+	struct config *ctx;
+	char *buf;
+
+	ctx = calloc(1, sizeof(*ctx));
+	buf = strdup(config);
+	config_parse(ctx, buf);
+	free(buf);
+	console_id = config_resolve_console_id(ctx, NULL);
+
+	assert(!config_get_value(ctx, "console-id"));
+	assert(config_get_value(ctx, "socket-id"));
+	assert(!strcmp(console_id, "socket"));
+
+	config_fini(ctx);
+}
+
+int main(void)
+{
+	test_independence_cmdline_optarg();
+	test_independence_config_console_id();
+	test_independence_config_socket_id();
+	test_independence_default();
+	test_precedence_cmdline_optarg();
+	test_precedence_config_console_id();
+	test_precedence_config_socket_id();
+
+	return EXIT_SUCCESS;
+}