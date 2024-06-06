3818a7a7d5b6b3676eaf3d0535c620dd04179dda
Alexander Hansen <alexander.hansen@9elements.com>
2024-06-06 15:34:37 +0200

    UART Mux Support: Add integration tests
    Add tests for logging, client r/w
    
    Change-Id: I4158c85c673fdc15bc5f8cae4bc601003b95e59a
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# taken from
+# https://github.com/amboar/dbus-sensors/commit/deef99485d7671634baee09aac6368b8b839b574
+
+# Support the docker container environment. OpenBMC relies on libsystemd's
+# sd_bus APIs, where sd_bus_default() relies on detection of a systemd
+# user-slice to determine whether the session or system bus is used. However, in
+# the container we do not appear to run in a user-slice, hence sd_bus_default()
+# hooks us up to the system bus.
+
+TEST="$1"
+
+export DBUS_SYSTEM_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS"
+
+"${TEST}"
