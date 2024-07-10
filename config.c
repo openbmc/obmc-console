openbmc/obmc-console

commit 1e04f449b7f00a7b426615533a44519149d2ea38

use iniparser dependency for config file parsing
For the config file, we do not need the custom handwritten parser.

Thanks to Andrew for this command, we can now search for an alternative

$ git grep -lw INI -- :/:*.bb
meta-openembedded/meta-oe/recipes-support/inih/libinih_58.bb
meta-openembedded/meta-oe/recipes-support/iniparser/iniparser_4.1.bb
meta-openembedded/meta-oe/recipes-support/minini/minini_1.2.b.bb
poky/meta/recipes-devtools/python/python3-iniconfig_2.0.0.bb
poky/meta/recipes-devtools/python/python3-iniparse_0.5.bb

For the ini parser we have following requirements

- small API
- easy to use
- compiles fast
- has tests, examples, docs
- support for sections

- minini [1]

  can be dropped from the list since it also supports colon
  ':' instead of '=' for separating key and value, creating 2 ways of
  doing something. This makes it harder to swap out the ini parser in
  the future.

- libinih [2]

  uses SAX-style parsing of .ini files and thus does not provide
  a DOM of the .ini. This is a break from the previous parser which
  stored everything in struct config. To use this library would require
  to create a struct to store all the possible configuration, then fill
  that struct in one pass. Essentially wrapping that library to have
  DOM capability. That would be possible, but not ideal. libinih is also
  highly configurable with lots of config options.

- iniparser [3]

  has all the required features and stores the results of its
  parsing for later use. It is a seamless upgrade from the previous
  parser. The call sites do not have to be modified and we can read the
  config as before. A downside is that we have to provide our own wrap
  file.

For our purposes, iniparser is a good choice.

Using this dependency allows us to drop the custom parser and all the
tests that go along with it.

If sections are required in future config files, iniparser can also
support that.

References:

[1] https://github.com/compuphase/minIni

config.c
@@ -29,127 +29,37 @@
 
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include "console-server.h"
+
+#include <iniparser/iniparser.h>
+
+#include "config.h"
+#include "config-internal.h"
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 static const char *config_default_filename = SYSCONFDIR "/obmc-console.conf";
 
-struct config_item {
-	char *name;
-	char *value;
-	struct config_item *next;
-};
-
-struct config {
-	struct config_item *items;
-};
-
 const char *config_get_value(struct config *config, const char *name)
 {
-	struct config_item *item;
+	int rc;
+	char buf[CONFIG_MAX_KEY_LENGTH];
+	rc = snprintf(buf, CONFIG_MAX_KEY_LENGTH, ":%s", name);
 
-	if (!config) {
+	if (rc < 0) {
 		return NULL;
 	}
 
-	for (item = config->items; item; item = item->next) {
-		if (!strcasecmp(item->name, name)) {
-			return item->value;
-		}
+	if ((size_t)rc >= sizeof(buf)) {
+		return NULL;
 	}
 
-	return NULL;
-}
+	const char *value = iniparser_getstring(config->dict, buf, NULL);
 
-static void config_parse(struct config *config, char *buf)
-{
-	struct config_item *item;
-	char *name;
-	char *value;
-	char *p;
-	char *line;
-
-	for (p = NULL, line = strtok_r(buf, "\n", &p); line;
-	     line = strtok_r(NULL, "\n", &p)) {
-		char *end;
-		int rc;
-
-		/* trim leading space */
-		while (isspace(*line)) {
-			line++;
-		}
-
-		/* skip comments */
-		if (*line == '#') {
-			continue;
-		}
-
-		name = malloc(strlen(line));
-		value = malloc(strlen(line));
-		if (name && value) {
-			rc = sscanf(line, "%[^ =] = %[^#]s", name, value);
-		} else {
-			rc = -ENOMEM;
-		}
-
-		if (rc != 2) {
-			free(name);
-			free(value);
-			continue;
-		}
-
-		/* trim trailing space */
-		end = value + strlen(value) - 1;
-		while (isspace(*end)) {
-			*end-- = '\0';
-		}
-
-		/* create a new item and add to our list */
-		item = malloc(sizeof(*item));
-		item->name = name;
-		item->value = value;
-		item->next = config->items;
-		config->items = item;
+	if (value && strlen(value) == 0) {
+		return NULL;
 	}
-}
 
-static struct config *config_init_fd(int fd, const char *filename)
-{
-	struct config *config;
-	size_t size;
-	size_t len;
-	ssize_t rc;
-	char *buf;
-
-	size = 4096;
-	len = 0;
-	buf = malloc(size + 1);
-	config = NULL;
-
-	for (;;) {
-		rc = read(fd, buf + len, size - len);
-		if (rc < 0) {
-			warn("Can't read from configuration file %s", filename);
-			goto out_free;
-
-		} else if (!rc) {
-			break;
-		}
-		len += rc;
-		if (len == size) {
-			size <<= 1;
-			buf = realloc(buf, size + 1);
-		}
-	}
-	buf[len] = '\0';
-
-	config = malloc(sizeof(*config));
-	config->items = NULL;
-
-	config_parse(config, buf);
-
-out_free:
-	free(buf);
-	return config;
+	return value;
 }
 
 struct config *config_init(const char *filename)
@@ -166,28 +76,30 @@ struct config *config_init(const char *filename)
 		warn("Can't open configuration file %s", filename);
 		return NULL;
 	}
-
-	config = config_init_fd(fd, filename);
-
 	close(fd);
+
+	dictionary *dict = iniparser_load(filename);
+
+	if (dict == NULL) {
+		return NULL;
+	}
+
+	config = malloc(sizeof(*config));
+	if (config) {
+		config->dict = dict;
+	}
 
 	return config;
 }
 
 void config_fini(struct config *config)
 {
-	struct config_item *item;
-	struct config_item *next;
-
 	if (!config) {
 		return;
 	}
 
-	for (item = config->items; item; item = next) {
-		next = item->next;
-		free(item->name);
-		free(item->value);
-		free(item);
+	if (config->dict) {
+		iniparser_freedict(config->dict);
 	}
 
 	free(config);
@@ -357,22 +269,3 @@ const char *config_resolve_console_id(struct config *config, const char *id_arg)
 
 	return DEFAULT_CONSOLE_ID;
 }
-
-#ifdef CONFIG_TEST
-int main(void)
-{
-	struct config_item *item;
-	struct config *config;
-
-	config = config_init_fd(STDIN_FILENO, "<stdin>");
-	if (!config)
-		return EXIT_FAILURE;
-
-	for (item = config->items; item; item = item->next)
-		printf("%s: %s\n", item->name, item->value);
-
-	config_fini(config);
-
-	return EXIT_SUCCESS;
-}
-#endif

.clang-tidy
@@ -207,7 +207,7 @@ readability-suspicious-call-argument,
 '
 
 WarningsAsErrors: '*'
-HeaderFilterRegex: '.*'
+HeaderFilterRegex: '^(test\/)?[a-zA-Z0-9]*\.h'
 CheckOptions:
   - { key: readability-identifier-naming.VariableCase,  value: lower_case }
   - { key: readability-identifier-naming.FunctionCase,  value: lower_case }

.gitignore
@@ -45,7 +45,6 @@ m4/lt~obsolete.m4
 
 # Repo specific items
 *.o
-/config.h
 /config.h.in~
 /config.log
 /config.status

config-internal.h
@@ -1,0 +1,25 @@
+/**
+ * Copyright © 2024 obmc-console authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <iniparser/dictionary.h>
+
+#define CONFIG_MAX_KEY_LENGTH 512
+
+struct config {
+	dictionary *dict;
+};

config.h
@@ -1,0 +1,34 @@
+/**
+ * Copyright © 2016 IBM
+ * Copyright © 2024 9elements
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <termios.h>
+
+struct config;
+
+const char *config_get_value(struct config *config, const char *name);
+struct config *config_init(const char *filename);
+const char *config_resolve_console_id(struct config *config,
+				      const char *id_arg);
+void config_fini(struct config *config);
+
+int config_parse_baud(speed_t *speed, const char *baud_string);
+uint32_t parse_baud_to_int(speed_t speed);
+speed_t parse_int_to_baud(uint32_t baud);
+int config_parse_bytesize(const char *size_str, size_t *size);

console-client.c
@@ -29,6 +29,7 @@
 #include <sys/un.h>
 
 #include "console-server.h"
+#include "config.h"
 
 #define EXIT_ESCAPE 2
 

console-dbus.c
@@ -21,6 +21,7 @@
 #include <sys/socket.h>
 
 #include "console-server.h"
+#include "config.h"
 
 /* size of the dbus object path length */
 const size_t dbus_obj_path_len = 1024;

console-server.c
@@ -39,6 +39,7 @@
 #include <poll.h>
 
 #include "console-server.h"
+#include "config.h"
 
 #define DEV_PTS_PATH "/dev/pts"
 

commit 628a45d7d07d1324d0eddae7cfcde7310783c041

config: Avoid unnecessary file TOCTOU race
iniparser_load() necessarily opens the file itself. The filesystem
may have changed state between the open-coded open(2) call and
iniparser_load(), which would yield an error result anyway.

If we were to retain this check it would be more useful to pass the
fd from the open(2) call through fdopen(3) to acquire a FILE handle,
which we can pass to iniparser_load_file(). But iniparser_load() already
implements this for us, so we may as well drop the open(2) call.

Even if we were unconcerned about the race, access(2) with `R_OK` is a
more effective approach than open(2) for checking permissions.

Change-Id: Icc04908e71d73b59e6df77eb192923d035f77dc5
Fixes: 1e04f449b7f0 ("use iniparser dependency for config file parsing")
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

config.c
@@ -65,21 +65,12 @@ const char *config_get_value(struct config *config, const char *name)
 struct config *config_init(const char *filename)
 {
 	struct config *config;
-	int fd;
 
 	if (!filename) {
 		filename = config_default_filename;
 	}
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0) {
-		warn("Can't open configuration file %s", filename);
-		return NULL;
-	}
-	close(fd);
-
 	dictionary *dict = iniparser_load(filename);
-
 	if (dict == NULL) {
 		return NULL;
 	}