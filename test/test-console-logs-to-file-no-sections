openbmc/obmc-console

commit 46d9ef298f2e26f4c513fedff3b8a26fdf4b2d35

Do not truncate log files after reboot
The log file will be truncated during the log_init process, so that the
logs will be lost after bmc(obmc-console) restarts.

Here we change the logic to loop through the sol logs instead of
truncating them when they already exist in the bmc system

Signed-off-by: John Wang <wangzhiqiang02@inspur.com>
Change-Id: I9a14867c7ec8bfb6f25edcc8f383afd44f8ee3ad

log-handler.c
@@ -62,7 +62,7 @@ static int log_trim(struct log_handler *lh)
 		/* don't return, as we need to re-open the logfile */
 	}
 
-	lh->fd = open(lh->log_filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	lh->fd = open(lh->log_filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (lh->fd < 0) {
 		warn("Can't open log buffer file %s", lh->log_filename);
 		return -1;
@@ -126,6 +126,29 @@ static enum ringbuffer_poll_ret log_ringbuffer_poll(void *arg, size_t force_len
 	return RINGBUFFER_POLL_OK;
 }
 
+static int log_create(struct log_handler *lh)
+{
+	off_t pos;
+
+	lh->fd = open(lh->log_filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (lh->fd < 0) {
+		warn("Can't open log buffer file %s", lh->log_filename);
+		return -1;
+	}
+	pos = lseek(lh->fd, 0, SEEK_CUR);
+	if (pos < 0) {
+		warn("Can't query log position for file %s", lh->log_filename);
+		close(lh->fd);
+		return -1;
+	}
+	lh->size = pos;
+	if ((size_t)pos >= lh->maxsize) {
+		return log_trim(lh);
+	}
+
+	return 0;
+}
+
 static int log_init(struct handler *handler, struct console *console,
 		    struct config *config)
 {
@@ -155,12 +178,6 @@ static int log_init(struct handler *handler, struct console *console,
 		filename = default_filename;
 	}
 
-	lh->fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (lh->fd < 0) {
-		warn("Can't open log buffer file %s", filename);
-		return -1;
-	}
-
 	lh->log_filename = strdup(filename);
 
 	rc = asprintf(&lh->rotate_filename, "%s.1", filename);
@@ -169,6 +186,10 @@ static int log_init(struct handler *handler, struct console *console,
 		return -1;
 	}
 
+	rc = log_create(lh);
+	if (rc < 0) {
+		return -1;
+	}
 	lh->rbc = console_ringbuffer_consumer_register(console,
 						       log_ringbuffer_poll, lh);
 

commit 366651d9ef0e03f97b1e1d2b6188f2b452044d1c

log-handler: Set the end of the file as the file size
The file descriptor is lost after system AC cycle.
The log file will grow indefinitely if the log recorded does not exceed
the maximum log size after each AC cycle.

Change-Id: If23d68b08817d54731dd6eff8a42e9eb476b6437
Fixes: 46d9ef298f2e ("Do not truncate log files after reboot")
Signed-off-by: Marshall Zhan <marshall.zhan.wiwynn@gmail.com>

log-handler.c
@@ -136,7 +136,7 @@ static int log_create(struct log_handler *lh)
 		warn("Can't open log buffer file %s", lh->log_filename);
 		return -1;
 	}
-	pos = lseek(lh->fd, 0, SEEK_CUR);
+	pos = lseek(lh->fd, 0, SEEK_END);
 	if (pos < 0) {
 		warn("Can't query log position for file %s", lh->log_filename);
 		close(lh->fd);

test/test-console-logs-to-file
@@ -26,13 +26,18 @@ trap cleanup EXIT
 
 TEST_CONF="${TEST_NAME}.conf"
 TEST_LOG="${TEST_NAME}.log"
+BLOCK_SIZE=1024
+LOG_MAX_SIZE=$((5 * $BLOCK_SIZE))
 
 cat <<EOF > "$TEST_CONF"
 active-console = $TEST_NAME
+logsize = $LOG_MAX_SIZE
 [$TEST_NAME]
 logfile = $TEST_LOG
 console-id = $TEST_NAME
 EOF
+
+dd if=/dev/zero bs=$BLOCK_SIZE count=$(($LOG_MAX_SIZE / $BLOCK_SIZE)) >> "$TEST_LOG"
 
 "$SOCAT" -u PTY,raw,echo=0,link=remote PTY,raw,echo=0,wait-slave,link=local &
 PTYS_PID="$!"
@@ -45,3 +50,5 @@ while ! busctl status --user xyz.openbmc_project.Console."${TEST_NAME}"; do slee
 echo console-should-log-to-file > remote
 sleep 1
 grep -LF console-should-log-to-file "$TEST_LOG"
+
+[ -e "$TEST_LOG" ] && [ $(stat -c%s "$TEST_LOG") -le $LOG_MAX_SIZE ]

test/test-console-logs-to-file-no-sections
@@ -26,11 +26,16 @@ trap cleanup EXIT
 
 TEST_CONF="${TEST_NAME}.conf"
 TEST_LOG="${TEST_NAME}.log"
+BLOCK_SIZE=1024
+LOG_MAX_SIZE=$((5 * $BLOCK_SIZE))
 
 cat <<EOF > "$TEST_CONF"
 logfile = $TEST_LOG
 console-id = $TEST_NAME
+logsize = $LOG_MAX_SIZE
 EOF
+
+dd if=/dev/zero bs=$BLOCK_SIZE count=$(($LOG_MAX_SIZE / $BLOCK_SIZE)) >> "$TEST_LOG"
 
 "$SOCAT" -u PTY,raw,echo=0,link=remote PTY,raw,echo=0,wait-slave,link=local &
 PTYS_PID="$!"
@@ -43,3 +48,5 @@ while ! busctl status --user xyz.openbmc_project.Console."${TEST_NAME}"; do slee
 echo console-should-log-to-file-legacy-syntax > remote
 sleep 1
 grep -LF console-should-log-to-file-legacy-syntax "$TEST_LOG"
+
+[ -e "$TEST_LOG" ] && [ $(stat -c%s "$TEST_LOG") -le $LOG_MAX_SIZE ]